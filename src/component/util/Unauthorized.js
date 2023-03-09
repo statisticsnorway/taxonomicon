68c69f85dada201e7ec2632af20756849c0bae00
eiliv <evd@ssb.no>
2023-03-09 15:44:27 +0100

    fix cat?

--- src/component/util/Unauthorized.js
+++ src/component/util/Unauthorized.js
@@ -2,11 +2,11 @@
 
 const Unauthorized = () =>
     <div style={{height: '100%'}} >
-        <div style={{marginTop: '50%', fontSize: '2rem', color: '#b5a2be'}}>
+        <div style={{marginTop: '25%', fontSize: '2rem', color: '#b5a2be'}}>
             Du har dessverre ikke tilgang :(
 
         </div>
-        <div><img height={400} width={400} src={'./sadcat.jpeg'}/></div>
+        <div><img height={400} width={400} src={'./assets/sadcat.jpeg'}/></div>
     </div>
 
 export default Unauthorized
